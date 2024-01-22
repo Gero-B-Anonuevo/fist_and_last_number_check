65db77f01939b45c9fb688330b6b63119144c687
Anonuevo <geroanonuevo3@gmail.com>
2024-01-22 15:18:34 +0800

    working

---
+++ prog.py
@@ -0,0 +1,15 @@
+#pseudocode
+#ask the numbers
+first_number = int(input("What is your first number: "))
+second_number = int(input("What is your second number: "))
+third_number = int(input("What is your third number: "))
+fourth_number = int(input("What is your fourth number: "))
+fifth_number = int(input("What is your fifth number: "))
+
+
+#compare first and last given number
+#true if identical and false if not
+if first_number == fifth_number:
+    print("True")
+else:
+    print("False")
