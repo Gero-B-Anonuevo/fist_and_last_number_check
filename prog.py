523f5e04f63321fd58ef23e96a7f5f06098156cb
Gero-B-Anonuevo <150780703+Gero-B-Anonuevo@users.noreply.github.com>
2024-01-22 23:17:49 +0800

    removing pseudocode, gitbash does not recognize removing pseudocode as a change in the fileUpdate prog.py

--- prog.py
+++ prog.py
@@ -1,5 +1,3 @@
-#pseudocode
-#ask the numbers
 first_number = int(input("What is your first number: "))
 second_number = int(input("What is your second number: "))
 third_number = int(input("What is your third number: "))
@@ -8,8 +6,6 @@ fifth_number = int(input("What is your fifth number: "))
 
 print("Given list: [", first_number, ", ", second_number, ", ", third_number, ", ", fourth_number, ", ", fifth_number, "]")
 
-#compare first and last given number
-#true if identical and false if not
 if first_number == fifth_number:
     print("Result is: True")
 else:
